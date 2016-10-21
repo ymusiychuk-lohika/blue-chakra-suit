7ebcab17864002f05961960f2e4f65cc5fd7495e
Yuriy Musiychuk <ymusiychuk@lohika.com>
2016-10-21 12:05:40 +0300

    Updated deploy script - running Benchmarks and benchmark analysis

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,2 +1,10 @@
+$env:RScript="c:\Program Files\R\R-3.3.1\bin\Rscript.exe"
+
 cd $env:APPLICATION_PATH
-Invoke-Expression 'cmd /c start powershell -Command { Write-Host "Profiling should run here..."; Start-Sleep -s 10 }'
+Write-Host "Starting profiling"
+
+git clone https://github.com/abodalevsky/node-cc-benchmark.git
+node .\node-cc-benchmark\compare.js --old "C:\Program Files\nodejs\node.exe" --new .\node.exe arrays assert buffers crypto domain es process querystring streams string_decoder util > .\compare-results.txt
+type .\compare-results.txt | & RScript .\node-cc-benchmark\compare.R > r-processed-results.txt
+
+Write-Host "Profiling finished"
