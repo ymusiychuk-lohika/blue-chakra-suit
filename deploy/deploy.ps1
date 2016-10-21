d319fc1d3e4f3f184dc8fc6eb4c575976bba38bd
Yuriy Musiychuk <ymusiychuk@lohika.com>
2016-10-21 12:14:24 +0300

    Updated deploy.ps1 to use "non-optimized" node-chakra

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -4,7 +4,7 @@ cd $env:APPLICATION_PATH
 Write-Host "Starting profiling"
 
 git clone https://github.com/abodalevsky/node-cc-benchmark.git
-node .\node-cc-benchmark\compare.js --old "C:\Program Files\nodejs\node.exe" --new .\node.exe arrays assert buffers crypto domain es process querystring streams string_decoder util > .\compare-results.txt
+node .\node-cc-benchmark\compare.js --old .\node-bin-old\node.exe --new .\node.exe arrays assert buffers crypto domain es process querystring streams string_decoder util > .\compare-results.txt
 type .\compare-results.txt | & RScript .\node-cc-benchmark\compare.R > r-processed-results.txt
 
 Write-Host "Profiling finished"
